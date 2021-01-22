67de03b712057652751dfe2bdf0eaeedcf349954
mohammed <lowray@protonmail.ch>
2021-01-22 21:05:57 +0200

    format v files

--- flags/mod.v
+++ flags/mod.v
@@ -1,61 +1,56 @@
 module flags
 
-
 pub type Callback = fn (options []string)
 
-pub struct Flag{
-	pub mut:
-		commands  map[string]Callback
-		options []string
-		prefix string
+pub struct Flag {
+pub mut:
+	commands map[string]Callback
+	options  []string
+	prefix   string
 }
 
-
-fn check_arg(this Flag, arg string) bool{
-	for key, callback in this.commands{
-		if arg == key{
+fn check_arg(this Flag, arg string) bool {
+	for key, callback in this.commands {
+		if arg == key {
 			callback(this.options)
 			return true
-		}else { return false}
+		} else {
+			return false
+		}
 	}
 }
 
-
-pub fn new(prefix string) Flag{
+pub fn new(prefix string) Flag {
 	c := map[string]Callback{}
 
-
 	return Flag{
-		commands : c
-		options : []
-		prefix : prefix
+		commands: c
+		options: []
+		prefix: prefix
 	}
 }
 
-
-pub fn (mut this Flag)  add_command(command string, callback Callback){
+pub fn (mut this Flag) add_command(command string, callback Callback) {
 	this.commands[command] = callback
 }
 
-
 pub fn (mut this Flag) parse(args []string) {
-	for arg in args{
-		if arg.len <= this.prefix.len{
-			if check_arg(this, arg){
+	for arg in args {
+		if arg.len <= this.prefix.len {
+			if check_arg(this, arg) {
 				break
-			}else{
-				panic("Unkown command ${arg}")
+			} else {
+				panic('Unkown command $arg')
 			}
-		}
-		else{
-			if arg[0..this.prefix.len] == this.prefix{
-				//flag
+		} else {
+			if arg[0..this.prefix.len] == this.prefix {
+				// flag
 				this.options << arg[this.prefix.len..]
-			}else{
-				if check_arg(this, arg){
+			} else {
+				if check_arg(this, arg) {
 					break
-				}else{
-					panic("Unkown command ${arg}")
+				} else {
+					panic('Unkown command $arg')
 				}
 			}
 		}
--- main.v
+++ main.v
@@ -1,25 +1,20 @@
 import flags
 import os
 
-
-
-
 fn main() {
 	mut f := flags.new('--')
 
-	f.add_command('hello', fn(options []string){
-			println('world')
+	f.add_command('hello', fn (options []string) {
+		println('world')
 	})
 
-	f.add_command('help', fn(options []string){
+	f.add_command('help', fn (options []string) {
 		mut msg := ''
-		if 'w' in options{msg = 'with message'}
-		println('help ${msg}')
+		if 'w' in options {
+			msg = 'with message'
+		}
+		println('help $msg')
 	})
-
-
-
 
 	f.parse(os.args[1..])
-
 }
